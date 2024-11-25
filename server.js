9089eef1f9459939267b81e7684881a960604d8b
Manuel Delpino <manudelpino0@gmail.com>
2024-11-24 22:46:20 -0300

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ io.on("connection", (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
